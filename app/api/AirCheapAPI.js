ee7aa220e870b67ecfbd0b9d695940470224f99a
wbp <wbpiggot@gmail.com>
2016-06-19 21:19:10 -0700

    Refactoring Dispatcher

--- app/api/AirCheapAPI.js
+++ app/api/AirCheapAPI.js
@@ -3,28 +3,13 @@ import AirportActionCreators from '../actions/AirportActionCreators.js';
 
 let AirCheapAPI = {
   fetchAirports() {
-    fetch('airports.json')
-    .then((response) => {
-      return response.json()
-    })
-    .then((responseData) => {
-      console.log(responseData)
-      AirportActionCreators.fetchAirportsSuccess(responseData)
-    })
-    .catch((error) => {
-      AirportActionCreators.fetchAirportsError(error);
-    });
+    return fetch('airports.json')
+    .then((response) => response.json())
   },
 
   fetchTickets() {
-    fetch('tickets.json')
+    return fetch('tickets.json')
     .then((response) => response.json())
-    .then((responseData) => {
-      AirportActionCreators.fetchTicketsSucess(responseData)
-    })
-    .catch((error) => {
-      AirportActionCreators.fetchTicketsError(error);
-    })
   }
 };
 
